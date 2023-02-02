86004f833c5edd185993b645fe6c8bbbf796f201
Elayaraja.K <kelaya5@gmail.com>
2023-02-02 21:03:16 +0530

    commit

---
+++ day2/Dev5.py
@@ -0,0 +1,38 @@
+number=0o1234567;
+print(number);
+number1=0xabcd123;
+print(number1);
+number3=180146467;
+print(type(number3))
+print(hex(number3));#converting decimal to hexadecimal using hex()
+number4=987654321;
+
+print("Bin conver : ",bin(number4)); # bin (it will conver to decimal to binary
+
+print("conver : ",hex(0b111010110111100110100010110001));
+
+print("conver : ",oct(0b111010110111100110100010110001));
+'''
+setValue1 = int(input("Student Marks1 : "));
+setValue2 = int(input("Student Marks2 : "));
+
+print(setValue1 is setValue2);
+print(setValue2);
+
+#int
+#bool  True or False;
+
+'''
+
+isStudent=False;
+
+print(type(isStudent));
+
+calc = 1.2e3;
+print(type(calc));
+calc = 20.33;
+print(calc)
+print(type(calc))
+
+
+
