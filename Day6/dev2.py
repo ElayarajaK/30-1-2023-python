b0297977c50a6a77fab836f4a36572c04267ab02
Elayaraja.K <kelaya5@gmail.com>
2023-02-07 21:00:03 +0530

    commit

---
+++ Day6/dev2.py
@@ -0,0 +1,15 @@
+S_name="raja";
+S_marks=100;
+S_name1="java";
+S_marks1=200;
+S_name2="python";
+S_marks2=300;
+print(10 > 5);
+print(S_marks > S_marks1)
+##     100    >  200
+print(S_marks < S_marks1)
+Name1="A";
+Name2="a";
+print("A" < "a")
+print("A" <"B")
+
