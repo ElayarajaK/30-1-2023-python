ad2fba4f43b5a11d41f9b8faf587a65a5f51fd67
Elayaraja.K <kelaya5@gmail.com>
2023-02-13 21:01:54 +0530

    commit

---
+++ Day8/dev.py
@@ -0,0 +1,23 @@
+Tamil=int(input("Tamil marks"));
+English=int(input("English marks"));
+Science=int(input("Tamil marks"));
+Math1=int(input("Tamil marks"));
+
+fixed_marks=4*100;
+total = Tamil+English+Science+Math1;
+avg = total/4;
+margin_marks=35;
+Result="";
+
+if( Tamil < margin_marks or English < margin_marks or Science < margin_marks or Math1 < margin_marks):
+    if(Tamil < margin_marks):
+        Result="Tamil marks",Tamil;
+elif(Tamil > margin_marks and English > margin_marks and Math1 > margin_marks and Science > margin_marks):
+    Total = Tamil+English+Math1+Science;
+    avg = Total/4;
+    print("Tamil : ",Tamil,"English : ",English ,"Science ",Science,"Total" ,Total," % ",avg)    
+    
+
+
+
+print(Result);
---
+++ Day8/dev1.py
@@ -0,0 +1,16 @@
+name = "Elayaraja";
+
+print("a" in name)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+print(name[6])
+print(name[7])
+print("==================")
+for temp in range(10):
+    print(temp)
+
+
