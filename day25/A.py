b9a639dbb720fa9be459543b6ffda42588d350cb
Raja <kelaya5@gmail.com>
2023-03-22 20:12:42 +0530

    commit

---
+++ day25/A.py
@@ -0,0 +1,41 @@
+name="Python full stack";
+##    012345
+print(name);
+print(name[0]);
+print(name[1:5])
+##      [startIndex:endIndex:jumpingIndex]
+print(name[5:20:3])
+print(name);
+
+print(name[::-1])
+
+print("hello"+"Hello 2")
+
+address="chennai";
+address1=address+" Tamilnadu";
+#print(address1+"banglore"+3)
+
+print(address1+"banglore"*3)
+
+print(address1)
+
+print("z" not in  address1)
+index =0;
+for temp in address1:
+    
+    print("String value is : ",temp,"in the index of : ",index);
+    index +=1;
+
+print(len(address1));
+print(address1)
+
+str="java";
+str1="python";
+print("abc"=="abc")
+print("abc" != "abc")
+print("abc" > "abb")
+
+
+
+lang=" my ";
+print(len(lang));
