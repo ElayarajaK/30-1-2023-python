2558d1b5b744fe299c8e58157a64d62fefecca9d
Raja <kelaya5@gmail.com>
2023-03-31 21:00:54 +0530

    commit

--- day28/D.py
+++ day28/D.py
@@ -8,4 +8,27 @@ values=10,20,30,40,50,"raja";
 print(type(values));
 print(values)
 print(values[1])
-values[1]=200;
+#values[1]=200;
+
+print(len(values))
+print(values[2:5:2])
+
+
+values2=30,10,5,60,70,2,60,2,60,2,70,10;
+print(sorted(values2,reverse=True));
+print(values2.count(70))
+print("min val : ",min(values2))
+
+print("max val : ",max(values2))
+
+print(sum(values2));
+
+print(values2.index(70))
+
+
+
+t1=10,20,3,0,40;
+t2=10,20,30,40;
+print(t1 == t2)
+
+
---
+++ day28/E.py
@@ -0,0 +1,27 @@
+values={10,20,30,40,"raja",20,40}
+
+print(type(values))
+print(values)
+
+values.add(True);
+
+print(values)
+
+values.add("40");
+
+print(values);
+
+values1={"ACE","Anna Univ","IIT"};
+
+values.update(values1);
+
+print(values);
+print(values1);
+
+print("==================")
+print(values)
+values.pop();
+print(values)
+
+values.discard(10);
+print(values)
